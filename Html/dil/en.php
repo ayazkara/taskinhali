f430a71791aea2902eb565f21522f28b9de6a8e9
ayazkara <ayazkara@gmail.com>
2017-11-02 10:11:02 +0300

    yeni

---
+++ Html/dil/en.php
@@ -0,0 +1,14 @@
+<?php 
+
+ $dil=array(
+   "anasayfa" => "Home",
+   "turlar"   => "Tours",
+   "galeri"   => "Gallery",
+   "iletisim" => "Contact",
+   "dilmenu"      => "Language",
+   "trdil"    => "Türkçe",
+   "endil"    => "English",
+    "populer"  => "Popular",
+   "harita"   => "Maps"
+ )
+?>
---
+++ Html/dil/tr.php
@@ -0,0 +1,14 @@
+<?php 
+
+ $dil=array(
+   "anasayfa" => "Anasayfa",
+   "turlar"   => "Turlar",
+   "galeri"   => "Galeri",
+   "iletisim" => "İletişim",
+   "dilmenu"      => "Dil",
+   "trdil"    => "Türkçe",
+   "endil"    => "English",
+   "populer"  => "Popüler",
+   "harita"   => "Harita"
+ )
+?>
